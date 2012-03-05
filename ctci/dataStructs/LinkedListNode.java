730b8414b6581b8901b5481cb2bc903de26d935a
Daniel Cashman <dan.a.cashman@gmail.com>
2012-03-05 01:48:32 -0800

    Linked list struct done and added

--- ctci/dataStructs/LinkedListNode.java
+++ ctci/dataStructs/LinkedListNode.java
@@ -1,3 +1,5 @@
+package dataStructs;
+
 public class LinkedListNode{
     private int data;
     private LinkedListNode next;
@@ -20,8 +22,18 @@ public class LinkedListNode{
 	return this.data;
     }
 
+    public void setData(int ddata){
+	this.data=data;
+	return;
+    }
+
     public LinkedListNode getNext(){
 	return this.next;
+    }
+
+    public void setNext(LinkedListNode next){
+	this.next=next;
+	return;
     }
   
     public void insertAfter(LinkedListNode next){
@@ -42,17 +54,31 @@ public class LinkedListNode{
 	return;
     }
 
-    public void insertBefore(LinkedListNode front){
+    public void insertListAfter(LinkedListNode next){
         if(next==null){
 	    System.out.print("No node inserted, erroneous node\n");
 	    return;
 	}
-        if(next.next!=null){
+	if(this.next==null){
+	    this.next=next;
+	}else{
+	    next.next=this.next;
+	    this.next=next;
+	}
+	return;
+    }
+
+    public LinkedListNode insertBefore(LinkedListNode front){
+        if(front==null){
+	    System.out.print("No node inserted, erroneous node\n");
+	    return this;
+	}
+        if(front.next!=null){
 	    System.out.print("front is not a single node, but rather a list\n");
-	    return;
+	    return this;
 	}      
         front.next=this;
-	return;
+	return front;
     }
 
     public LinkedListNode removeFront(){
@@ -64,6 +90,16 @@ public class LinkedListNode{
 	    return temp;
 	}
     } 
+
+    public String toString(){
+        StringBuffer manipString = new StringBuffer();
+        LinkedListNode explorer = this;
+	while(explorer!=null){
+	    manipString.append(explorer.data+", ");
+            explorer=explorer.next;
+	}
+	return new String(manipString);
+    }
 
 
 
