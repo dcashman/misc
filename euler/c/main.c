84c8ae186e46a2151623540974acb36b04babcd9
dcashman <dan.a.cashman@gmail.com>
2014-01-09 22:31:29 -0800

    First commit.

---
+++ euler/c/main.c
@@ -0,0 +1,11 @@
+/* main routine for C versions of project euler problems: 
+ *  http://projecteuler.net/
+ *  Author: Dan Cashman
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h
+
+int main(int argc, char* argv[]){
+    return 0;
+}
